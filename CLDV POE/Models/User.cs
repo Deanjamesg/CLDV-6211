27463d9e537218b0ea80b43e2444574c2490b79e
Dean James <st10378305@vcconnect.edu.za>
2024-05-01 13:14:51 +0200

    Recreated Repository for Web App
    I had to recreate the repository on GitHub due to security issues. So I have created a Azure SQL Server, Azure Database and I have connected everything to my Web App.

---
+++ CLDV POE/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CLDV_POE.Models
+{
+    public class User : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
